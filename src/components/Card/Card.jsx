a92dcb6051c81ff2bd405fdefe51496339921562
Lesya Kosilova <lesyaaprok@gmail.com>
2022-12-20 21:58:24 +0300

    [MG-3] feat: add an ability to choose two cards and change their state

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import styles from './Card.module.css';
 
-const Card = ({ imgSrc }) => {
+const Card = ({ imgSrc, isOpen, onClick }) => {
   const image = require(`../../images/${imgSrc}`);
+  const activeClass = isOpen ? `${styles.active}` : '';
+  const activeBack = isOpen ? `${styles.activeBack}` : '';
 
   return (
-    <div className={styles.card}>
-      <img className={styles.img} src={image} alt="" />
-      <div className={styles.back} />
-    </div>
+    <button
+      type="button"
+      className={`${styles.card} ${activeClass}`}
+      onClick={() => onClick()}
+    >
+      <div className={styles.front}>
+        <img className={styles.img} src={image} alt="" />
+      </div>
+      <div className={`${activeBack} ${styles.back}`} />
+    </button>
   );
 };
 
